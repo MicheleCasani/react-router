cc4de3428f80c95d2521afd50843b15e982070fd
MicheleCasani <casanimichele@gmail.com>
2025-05-15 14:26:59 +0200

    [UPD]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import HomePage from './pages/HomePage';
+import AboutUs from './pages/AboutUs';
+import Products from './pages/Products';
 
 function App() {
 
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={HomePage}></Route>
+          <Route path='/about' Component={AboutUs}></Route>
+          <Route path='/products' Component={Products}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
