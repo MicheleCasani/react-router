124295830d84d2ef3db5a44e3267b64be0d2b937
MicheleCasani <casanimichele@gmail.com>
2025-05-16 14:46:10 +0200

    [UPD]

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -4,19 +4,45 @@ import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 
 
-function DetaiProduct() {
+
+
+function DetailProduct() {
+
+    const { id } = useParams();
+
+    const [product, setProduct] = useState([]);
+
+    const getProduct = () => {
+        axios
+            .get(`https://fakestoreapi.com/products/${id}`)
+            .then((response) => {
+                setProduct(response.data)
+            })
+    }
+
+    useEffect(() => {
+        getProduct();
+    }, [])
+
     return (
         <div>
             <h1>Dettaglio Prodotto</h1>
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        <div className="card">
+                        <div className="card flex-fill my-3">
                             <div className="card-img-top">
-
+                                <img className='img-fluid' src={product.image} alt="" />
                             </div>
                             <div className="card-body">
-
+                                <h1>{product.title}</h1>
+                                <span>
+                                    <em>{product.price}€</em>
+                                </span>
+                                <span>
+                                    <strong>{product.category}</strong>
+                                </span>
+                                <p>{product.description}</p>
                             </div>
                         </div>
                     </div>
@@ -26,4 +52,4 @@ function DetaiProduct() {
     )
 }
 
-export default DetaiProduct
+export default DetailProduct
