05688f7e592fdd50e0474e13bae5d680d63b9dc5
MicheleCasani <casanimichele@gmail.com>
2025-05-16 14:25:55 +0200

    [UPD]

---
+++ src/pages/DetaiProduct.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+
+function DetaiProduct() {
+    return (
+        <div>
+            <h1>Dettaglio Prodotto</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card">
+                            <div className="card-img-top">
+
+                            </div>
+                            <div className="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DetaiProduct
